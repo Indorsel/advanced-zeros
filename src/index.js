a7e6c75401415e8933fe3ba7bf5a89672725a7e8
Tsimur Mirzayeu <timoha993@gmail.com>
2019-03-24 18:46:49 +0300

    feat: create a solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let factorial = number;
+  let baseSystem = base;
+  
+  for (let i = 2; i <= baseSystem; i++) {
+    let answer = 0;
+    while (baseSystem % i === 0) {
+      baseSystem = Math.floor(baseSystem / i); 
+      answer++;
+    }
+
+    let zeros = 0;
+    let count = number;
+    while (count / i > 0) {
+      zeros += Math.floor(count / i); 
+      count = Math.floor(count / i);
+    }
+    factorial = Math.floor(Math.min(factorial, zeros / answer));
+  }
+
+  return factorial;
 }
